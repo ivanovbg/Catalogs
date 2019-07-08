ad3c2ba59870605db1bdfc642f0c36abdcb8cbab
Krasimir Petrov <ivanovkbg@gmail.com>
2019-07-08 20:54:41 +0300

    --Code refactor

--- app/src/main/java/com/softomotion/catalogs/data/database/AppDatabase.java
+++ app/src/main/java/com/softomotion/catalogs/data/database/AppDatabase.java
@@ -1,10 +1,10 @@
 package com.softomotion.catalogs.data.database;
 
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
+        import androidx.room.Database;
+        import androidx.room.RoomDatabase;
 
-import com.softomotion.catalogs.data.database.daos.BrochureDao;
-import com.softomotion.catalogs.data.database.entities.Brochure;
+        import com.softomotion.catalogs.data.database.daos.BrochureDao;
+        import com.softomotion.catalogs.data.database.entities.Brochure;
 
 @Database(entities = {Brochure.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
